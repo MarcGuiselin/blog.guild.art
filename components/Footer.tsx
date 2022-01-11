86d89a5de4e49910bf3792dba4edce7b45b5085c
MarcGuiselin <24906387+MarcGuiselin@users.noreply.github.com>
2022-01-11 14:47:59 -0500

    feat: copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,11 +10,8 @@ export default function Footer() {
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
         </div>
-        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
-          <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
+        <div className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+          {`© ${new Date().getFullYear()} `}
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
