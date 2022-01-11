b3ecea16c680b72887a1bea50a77d71de623f432
MarcGuiselin <24906387+MarcGuiselin@users.noreply.github.com>
2022-01-11 14:44:44 -0500

    feat: metadata corrections

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,12 +1,12 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'The Guild of Artists',
+  author: '@guildofartists',
+  headerTitle: 'The Guild of Artists Blog',
+  description: 'A blog for the wider community of independent artists',
   language: 'en-us',
-  theme: 'system', // system, dark or light
+  theme: 'dark', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: '',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
