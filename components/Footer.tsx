8f27a9add55c278b7a028d504e1cb2299b21ac3d
MarcGuiselin <24906387+MarcGuiselin@users.noreply.github.com>
2022-01-11 18:10:39 -0500

    feat: remove link to github theme

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,11 +14,6 @@ export default function Footer() {
           {`© ${new Date().getFullYear()} `}
           <Link href="/">{siteMetadata.title}</Link>
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
-        </div>
       </div>
     </footer>
   )
