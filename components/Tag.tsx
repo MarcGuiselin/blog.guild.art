411bf242255a54719478743170e48ca631e5d0b7
MarcGuiselin <24906387+MarcGuiselin@users.noreply.github.com>
2022-01-11 21:12:39 -0500

    feat: rounded tags

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -5,10 +5,12 @@ interface Props {
   text: string
 }
 
+// mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400
+
 const Tag = ({ text }: Props) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+      <a className="mr-2 mt-1 transition text-sm font-medium tracking-wide capitalize text-gray-800 dark:text-white bg-neutral-200 dark:bg-onyx-700 px-[7px] py-[1px] rounded-full dark:hover:bg-primary-500">
         {text.split(' ').join('-')}
       </a>
     </Link>
