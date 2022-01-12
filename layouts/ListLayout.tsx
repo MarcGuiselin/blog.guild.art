eb96679519aeeb825f399109bc97ae142fd6a66f
MarcGuiselin <24906387+MarcGuiselin@users.noreply.github.com>
2022-01-11 20:43:11 -0500

    feat: use display font for blogpost titles

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -68,7 +68,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
                   </dl>
                   <div className="space-y-3 xl:col-span-3">
                     <div>
-                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
+                      <h3 className="text-2xl font-bold leading-8 tracking-wide font-display">
                         <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
                           {title}
                         </Link>
