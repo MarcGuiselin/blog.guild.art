b198cbc8c643a1dff67d28d789486183e154c399
MarcGuiselin <24906387+MarcGuiselin@users.noreply.github.com>
2022-01-19 22:39:54 -0500

    feat: static tweets look more like twitter embed

--- components/EmbedTweet/index.tsx
+++ components/EmbedTweet/index.tsx
@@ -68,7 +68,7 @@ export default function EmbedTweet({
   const formattedText = text.replace(/https:\/\/[\n\S]+/g, '')
 
   return (
-    <div className="not-prose tweet rounded-lg border border-gray-200 dark:border-transparent px-6 py-4 my-4 w-full bg-white dark:bg-onyx-800">
+    <div className="not-prose tweet rounded-lg border border-slate-300 dark:border-onyx-700 px-6 py-4 my-4 w-full bg-white dark:bg-onyx-800 dark:bg-opacity-0 dark:hover:bg-opacity-100 transition">
       <div className="flex items-center">
         <a className="flex h-12 w-12" href={authorUrl} target="_blank" rel="noopener noreferrer">
           <Image
@@ -83,10 +83,10 @@ export default function EmbedTweet({
           href={authorUrl}
           target="_blank"
           rel="noopener noreferrer"
-          className="author flex flex-col ml-4"
+          className="author flex flex-col ml-4 group"
         >
           <span
-            className="flex items-center font-bold !text-gray-900 dark:!text-gray-100 leading-5"
+            className="flex items-center font-bold text-slate-900 dark:text-gray-100 leading-5 group-hover:!underline"
             title={author.name}
           >
             <Twemoji text={author.name} />
@@ -102,11 +102,14 @@ export default function EmbedTweet({
               </svg>
             ) : null}
           </span>
-          <span className="!text-gray-500" title={`@${author.id}`}>
+          <span
+            className="text-slate-500 dark:text-white dark:text-opacity-50 tracking-wide"
+            title={`@${author.id}`}
+          >
             @{author.id}
           </span>
         </a>
-        <a className="ml-auto" href={authorUrl} target="_blank" rel="noopener noreferrer">
+        <a className="ml-auto" href={tweetUrl} target="_blank" rel="noopener noreferrer">
           <svg viewBox="328 355 335 276" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M 630, 425    A 195, 195 0 0 1 331, 600    A 142, 142 0 0 0 428, 570    A  70,  70 0 0 1 370, 523    A  70,  70 0 0 0 401, 521    A  70,  70 0 0 1 344, 455    A  70,  70 0 0 0 372, 460    A  70,  70 0 0 1 354, 370    A 195, 195 0 0 0 495, 442    A  67,  67 0 0 1 611, 380    A 117, 117 0 0 0 654, 363    A  65,  65 0 0 1 623, 401    A 117, 117 0 0 0 662, 390    A  65,  65 0 0 1 630, 425    Z"
@@ -117,7 +120,7 @@ export default function EmbedTweet({
       </div>
       <Twemoji
         tag="div"
-        className="mt-4 mb-1 leading-normal whitespace-pre-wrap text-lg !text-gray-700 dark:!text-gray-300"
+        className="mt-4 mb-1 leading-normal whitespace-pre-wrap text-xl !text-slate-700 dark:!text-slate-300"
         text={formattedText}
       />
       {media && media.length ? (
@@ -146,19 +149,14 @@ export default function EmbedTweet({
         </div>
       ) : null}
       {quoteTweet ? <EmbedTweet data={quoteTweet} /> : null}
-      <a
-        className="!text-gray-500 text-sm"
-        href={tweetUrl}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a className="!text-slate-500" href={tweetUrl} target="_blank" rel="noopener noreferrer">
         <time title={`Time Posted: ${date.toUTCString()}`} dateTime={date.toISOString()}>
           {formatDate(date)}
         </time>
       </a>
-      <div className="flex !text-gray-700 dark:!text-gray-300 mt-2">
+      <div className="flex !text-slate-700 dark:!text-slate-300 mt-2 pt-3 border-t border-slate-300 dark:border-onyx-700">
         <a
-          className="flex items-center mr-4 !text-gray-500 hover:!text-blue-600 transition hover:!underline"
+          className="flex items-center mr-4 !text-slate-500 hover:!text-blue-600 transition hover:!underline"
           href={replyUrl}
           target="_blank"
           rel="noopener noreferrer"
@@ -172,7 +170,7 @@ export default function EmbedTweet({
           <ShowStat count={replyCount} />
         </a>
         <a
-          className="flex items-center mr-4 !text-gray-500 hover:!text-green-600 transition hover:!underline"
+          className="flex items-center mr-4 !text-slate-500 hover:!text-green-600 transition hover:!underline"
           href={retweetUrl}
           target="_blank"
           rel="noopener noreferrer"
@@ -186,7 +184,7 @@ export default function EmbedTweet({
           <ShowStat count={retweetCount} />
         </a>
         <a
-          className="flex items-center !text-gray-500 hover:!text-red-600 transition hover:!underline"
+          className="flex items-center !text-slate-500 hover:!text-red-600 transition hover:!underline"
           href={likeUrl}
           target="_blank"
           rel="noopener noreferrer"
