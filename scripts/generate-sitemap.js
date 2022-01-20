a382b2c9c88985e21f3903e9b605f7e10221ebd5
MarcGuiselin <24906387+MarcGuiselin@users.noreply.github.com>
2022-01-19 19:32:01 -0500

    fix: sitemap being incorrectly generated

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -4,44 +4,35 @@ const prettier = require('prettier')
 const siteMetadata = require('../data/siteMetadata')
 
 ;(async () => {
-  const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
-  const pages = await globby([
-    'pages/*.js',
-    'data/blog/**/*.mdx',
-    'data/blog/**/*.md',
-    'public/tags/**/*.xml',
-    '!pages/_*.js',
-    '!pages/api',
+  const [prettierConfig, defaultPages, tagPages, blogPages, authorPages] = await Promise.all([
+    prettier.resolveConfig('./.prettierrc.js'),
+    globby(['pages/**', '!pages/**_**', '!pages/api/**', '!pages/404*', '!**/\\[*\\].*']),
+    globby('public/blog/tags/**/*.xml'),
+    globby('data/blog/**/*'),
+    globby('data/author/**/*'),
   ])
 
-  const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${pages
-              .map((page) => {
-                const path = page
-                  .replace('pages/', '/')
-                  .replace('data/blog', '/blog')
-                  .replace('public/', '/')
-                  .replace('.js', '')
-                  .replace('.mdx', '')
-                  .replace('.md', '')
-                  .replace('/feed.xml', '')
-                const route = path === '/index' ? '' : path
-                if (page === `pages/404.js` || page === `pages/blog/[...slug].js`) {
-                  return
-                }
-                return `
-                        <url>
-                            <loc>${siteMetadata.siteUrl}${route}</loc>
-                        </url>
-                    `
-              })
-              .join('')}
-        </urlset>
-    `
+  const pages = [
+    ...defaultPages.map((page) => page.replace(/pages\/(index)?|\/index/g, '')),
+    ...tagPages.map((page) => page.replace(/^public\/|\/feed/g, '')),
+    ...blogPages.map((page) => page.replace('data/', '')),
+    ...authorPages.map((page) => page.replace('data/', 'blog/')),
+  ].map((m) => m.replace(/\.[^/\\]+$/, ''))
 
-  const formatted = prettier.format(sitemap, {
+  // Order by path depth first, then alphabetically
+  const pathComp = (a) => Math.min(a.split('/').length, 2)
+  pages.sort((a, b) => pathComp(a) - pathComp(b) || a.localeCompare(b))
+
+  console.log(pages)
+
+  const sitemap = [
+    '<?xml version="1.0" encoding="UTF-8"?>',
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+    ...pages.flatMap((page) => ['<url>', `<loc>${siteMetadata.siteUrl}${page}</loc>`, '</url>']),
+    '</urlset>',
+  ]
+
+  const formatted = prettier.format(sitemap.join(''), {
     ...prettierConfig,
     parser: 'html',
   })
