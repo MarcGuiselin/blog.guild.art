8d8d87fe56308b7d41fcf9a3d34440ec9c857ff6
MarcGuiselin <24906387+MarcGuiselin@users.noreply.github.com>
2022-01-19 19:57:04 -0500

    fix: add missing backslash in sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -26,7 +26,7 @@ const siteMetadata = require('../data/siteMetadata')
   const sitemap = [
     '<?xml version="1.0" encoding="UTF-8"?>',
     '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
-    ...pages.flatMap((page) => ['<url>', `<loc>${siteMetadata.siteUrl}${page}</loc>`, '</url>']),
+    ...pages.flatMap((page) => ['<url>', `<loc>${siteMetadata.siteUrl}/${page}</loc>`, '</url>']),
     '</urlset>',
   ]
 
