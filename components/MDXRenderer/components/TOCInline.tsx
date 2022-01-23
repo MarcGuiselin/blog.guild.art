7478fcb1fc3b1e50e5ed868d7d4118260fc887d6
MarcGuiselin <24906387+MarcGuiselin@users.noreply.github.com>
2022-01-23 15:43:56 -0500

    feat: improved TOC

--- components/MDXRenderer/components/TOCInline.tsx
+++ components/MDXRenderer/components/TOCInline.tsx
@@ -42,13 +42,20 @@ const TOCInline = ({
   )
 
   const tocList = (
-    <ul>
-      {filteredToc.map((heading) => (
-        <li key={heading.value} className={`${heading.depth >= indentDepth && 'ml-6'}`}>
-          <a href={heading.url}>{heading.value}</a>
-        </li>
-      ))}
-    </ul>
+    <div className="not-prose flex justify-center">
+      <div className="py-4 px-6 bg-slate-50 dark:bg-onyx-800 rounded-lg shadow-lg">
+        <h2 className="text-center text-lg font-extrabold tracking-tight text-gray-900 dark:text-gray-100 md:text-xl pb-4">
+          Skip to a part of the Article
+        </h2>
+        <ul>
+          {filteredToc.map((heading) => (
+            <li key={heading.value} className={`${heading.depth >= indentDepth && 'ml-6'}`}>
+              <a href={heading.url}>{heading.value}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 
   return (
