e3a69285a6a84509a1519fa8cc1da2bc5680890b
MarcGuiselin <24906387+MarcGuiselin@users.noreply.github.com>
2022-01-28 17:07:54 -0500

    feat: improvement to tailor form title

--- components/MDXRenderer/components/Tailor/Form.tsx
+++ components/MDXRenderer/components/Tailor/Form.tsx
@@ -1,23 +1,34 @@
 import { ReactNode } from 'react'
 import { useChoices } from './context'
+import Twemoji from '@/components/Twemoji'
 
 type Props = {
+  title?: string
+  subtitle?: string
   children: ReactNode
 }
 
-export default function Form({ children }: Props) {
+export default function Form({
+  title = 'Use this tool to shorten the article! 🤟',
+  subtitle = 'Answer the questions below to get started',
+  children,
+}: Props) {
   const { clear, changed } = useChoices()
   return (
     <div className="relative px-0 sm:px-4 py-4 not-prose" role="menu">
       <div className="z-0 absolute top-2 bottom-2 -left-6 -right-6 rounded-3xl bg-rainbow opacity-10 blur-xl translate-y-[0px]" />
       <div className="z-10 relative bg-slate-50 dark:bg-onyx-700 px-4 py-2 rounded-lg space-y-4 shadow-lg">
         <div className="text-center">
-          <h2 className="font-display text-lg sm:text-xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
-            Use this tool to shorten the article
-          </h2>
-          <p className="text-sm sm:text-base font-normal text-gray-500 dark:text-gray-400">
-            Answer the questions below to get started
-          </p>
+          <Twemoji
+            tag="h2"
+            className="font-display text-lg sm:text-xl font-bold text-gray-900 dark:text-gray-100"
+            text={title}
+          />
+          <Twemoji
+            tag="p"
+            className="text-sm sm:text-base font-normal text-gray-500 dark:text-gray-400"
+            text={subtitle}
+          />
         </div>
         <div
           className="py-2 sm:px-6 space-y-4 divide-y divide-slate-300 dark:divide-onyx-900"
